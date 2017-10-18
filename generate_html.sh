687aa8b4a1373dc7bca77f888cfb75227895cbc2
varemo <leifvaremo+git@gmail.com>
2017-10-18 17:25:44 +0200

    fix duplicate plot in html

--- generate_html.sh
+++ generate_html.sh
@@ -3,4 +3,5 @@ Rscript -e 'rmarkdown::render("functional_annotation.Rmd")'
 
 # Make a copy HTML file with answers displayed:
 cp functional_annotation.html functional_annotation_answers.html
+# This may only work for Mac (-e option replaced by "")
 sed -i "" "s/div.answer { display: none;}/div.answer { display: true;}/g" functional_annotation_answers.html
