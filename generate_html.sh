5fa08b97f2299292ec78f3ca34692f5212d6b42f
varemo <leifvaremo+git@gmail.com>
2017-10-30 16:42:37 +0100

    add html build and push to github

--- generate_html.sh
+++
@@ -1,7 +0,0 @@
-# Knit HTML:
-Rscript -e 'rmarkdown::render("functional_annotation.Rmd")'
-
-# Make a copy HTML file with answers displayed:
-cp functional_annotation.html functional_annotation_answers.html
-# This may only work for Mac (-e option replaced by "")
-sed -i "" "s/div.answer { display: none;}/div.answer { display: true;}/g" functional_annotation_answers.html
